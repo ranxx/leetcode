c77972acc9a52af667235a5ba79f619a554edcda
ranxx <ran.star@foxmail.com>
2020-07-06 17:00:29 +0800

    2. 两数相加

--- go/2.两数相加.go
+++ go/2.两数相加.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
  * @lc app=leetcode.cn id=2 lang=golang
  *
@@ -8,11 +12,11 @@ package main
  * https://leetcode-cn.com/problems/add-two-numbers/description/
  *
  * algorithms
- * Medium (36.84%)
- * Likes:    4020
+ * Medium (37.69%)
+ * Likes:    4544
  * Dislikes: 0
- * Total Accepted:    351.7K
- * Total Submissions: 954K
+ * Total Accepted:    467.9K
+ * Total Submissions: 1.2M
  * Testcase Example:  '[2,4,3]\n[5,6,4]'
  *
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -30,19 +34,15 @@ package main
  *
  */
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+/*
+ * @解题思路
+ *
+ * 根据题意可知：链表翻转表示一个数字
+ * 实际上我们可以从两个链表的头往后面相加就行了，合大于等于10就向后进1
+ * 最后如果哪个链表剩下那么全部赋值过来就行了
+ */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	root := &ListNode{}
 	tmp := root
@@ -50,34 +50,61 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	for l1 != nil || l2 != nil {
 		num := 0
 		if flag {
-			flag = false
-			num++
+			flag, num = false, num+1
 		}
 		if l1 != nil {
-			num += l1.Val
-			l1 = l1.Next
+			num, l1 = num+l1.Val, l1.Next
 		}
 		if l2 != nil {
-			num += l2.Val
-			l2 = l2.Next
+			num, l2 = num+l2.Val, l2.Next
 		}
 		if num >= 10 {
-			flag = true
-			num = num - 10
+			flag, num = true, num-10
 		}
-		tmp.Next = &ListNode{
-			Val: num,
-		}
+		tmp.Next = &ListNode{Val: num}
 		tmp = tmp.Next
 	}
 
 	if flag {
-		tmp.Next = &ListNode{
-			Val: 1,
-		}
+		tmp.Next = &ListNode{Val: 1}
 	}
 
 	return root.Next
 }
 
 // @lc code=end
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func initListNode(a ...int) *ListNode {
+	head := &ListNode{}
+	tmp := head
+	for _, v := range a {
+		tmp.Next = &ListNode{
+			Val: v,
+		}
+		tmp = tmp.Next
+	}
+	return head.Next
+}
+
+func (l *ListNode) String() string {
+	ret := ""
+	for tmp := l; tmp != nil; tmp = tmp.Next {
+		ret = fmt.Sprintf("%v", tmp.Val) + ret
+	}
+	return ret
+}
+
+func main() {
+	l1 := initListNode(9, 9, 9)
+	l2 := initListNode(9, 9, 9)
+	l3 := addTwoNumbers(l1, l2)
+	fmt.Println(l1)
+	fmt.Println(l2)
+	fmt.Println("+")
+	fmt.Println(l3)
+}
